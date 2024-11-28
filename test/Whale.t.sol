311b0359b5576fe57acb6171ecb59300883280d5
Nejc Schneider <nejc.sch@gmail.com>
2024-11-28 13:06:08 +0100

    tests: fork tokens

---
+++ test/Whale.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/interfaces/IERC20.sol";
+
+contract ForkTest is Test {
+    IERC20 public dai;
+
+    function setUp() public {
+        // Main-net DAI address.
+        dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    }
+
+    function testDeposit() public {
+        address alice = address(123);
+
+        uint256 balanceBefore = dai.balanceOf(alice);
+        console.log("balance before", balanceBefore);
+
+        uint256 totalBefore = dai.totalSupply();
+        console.log("total supply before", totalBefore / 1e18);
+
+        // token, account, amount, adjust total supply
+        deal(address(dai), alice, 1e6 * 1e18, true);
+
+        uint256 balanceAfter = dai.balanceOf(alice);
+        console.log("balance after", balanceAfter / 1e18);
+
+        uint256 totalAfter = dai.totalSupply();
+        console.log("total supply after", totalAfter / 1e18);
+    }
+}
