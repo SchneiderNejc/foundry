9dbdd07542b23624886086c791c8640e8b21c769
Nejc Schneider <nejc.sch@gmail.com>
2024-11-28 12:45:29 +0100

    add associated contracts

---
+++ src/GaslessTokenTransfer.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.8.27;
+
+import "../src/interfaces/IERC20Permit.sol";
+
+contract GaslessTokenTransfer {
+    function send(
+        address token,
+        address sender,
+        address receiver,
+        uint amount,
+        uint fee,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        IERC20Permit(token).permit(
+            sender,
+            address(this),
+            amount + fee,
+            deadline,
+            v,
+            r,
+            s
+        );
+        IERC20Permit(token).transferFrom(sender, receiver, amount);
+        IERC20Permit(token).transferFrom(sender, msg.sender, fee);
+    }
+}
